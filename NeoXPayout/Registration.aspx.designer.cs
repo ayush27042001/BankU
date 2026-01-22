eb41dae2bda0a7d10c857a284021280bb2b2affe
Sachin <sachinjha2002@gmail.com>
2026-01-22 18:02:19 +0530

    Registration Correction

--- NeoXPayout/Registration.aspx.designer.cs
+++ NeoXPayout/Registration.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace NeoXPayout
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// btnBack control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnBack;
+
+        /// <summary>
         /// lblPanName control.
         /// </summary>
         /// <remarks>
